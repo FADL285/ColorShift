ee359c829ccae0804e329b9c5e040ee2f8f3b065
Mohamed Fadl <mohamed@fadl.info>
2025-12-08 22:47:23 +0200

    chore: update project configuration and dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  modules: ['@nuxt/eslint', '@nuxt/hints', '@nuxt/ui', '@nuxt/test-utils', '@vueuse/nuxt'],
+
   devtools: { enabled: true },
 
-  modules: ['@nuxt/eslint', '@nuxt/hints', '@nuxt/ui', '@nuxt/test-utils']
+  css: ['~/assets/css/main.css'],
+
+  components: [{ path: '~/components', pathPrefix: false }],
+
+  compatibilityDate: '2025-07-15'
 })
